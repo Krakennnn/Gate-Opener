9a99f0bb14f0551095301343ac4f4f74c35dcde7
Krakennnn <41086710+Krakennnn@users.noreply.github.com>
2026-08-02 16:33:09 -0400

    Rev2
    Added relays for second actuator and programed the outputs. Initial test seemed good

---
+++ GateOpenerProgram/GateOpenerProgramExport/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/LLSketch_build/core/api/Common.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\wjas0\Downloads\GateOpener\LLSketch_build\core\api\Common.cpp.o: \
+ C:\Users\wjas0\AppData\Local\T\A\internal\arduino_mbed_opta_4.1.6_6bd5cf0253f43b1d\cores\arduino\api\Common.cpp \
+ C:\Users\wjas0\AppData\Local\T\A\internal\arduino_mbed_opta_4.1.6_6bd5cf0253f43b1d\cores\arduino\api\Common.h
---
+++ GateOpenerProgram/GateOpenerProgramExport/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/LLSketch_build/core/random_seed.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\wjas0\Downloads\GateOpener\LLSketch_build\core\random_seed.cpp.o: \
+ C:\Users\wjas0\AppData\Local\T\A\internal\arduino_mbed_opta_4.1.6_6bd5cf0253f43b1d\cores\arduino\random_seed.cpp
