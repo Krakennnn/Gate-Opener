fe29551f7b03b6852e35ba5a91b82176d7e2e742
Krakennnn <41086710+Krakennnn@users.noreply.github.com>
2026-08-02 20:44:34 -0400

    Revert "Rev2"
    This reverts commit 9a99f0bb14f0551095301343ac4f4f74c35dcde7.

--- GateOpenerProgram/GateOpenerProgramExport/Rev2With4Relays/LLSketch_build/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\wjas0\Downloads\GateOpener\LLSketch_build\core\abi.cpp.o: \
- C:\Users\wjas0\AppData\Local\T\A\internal\arduino_mbed_opta_4.1.6_6bd5cf0253f43b1d\cores\arduino\abi.cpp
--- GateOpenerProgram/GateOpenerProgramExport/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/Rev2With4Relays/LLSketch_build/libraries/Arduino_Opta_Blueprint/OptaCrc.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\wjas0\Downloads\GateOpener\LLSketch_build\libraries\Arduino_Opta_Blueprint\OptaCrc.cpp.o: \
- C:\Users\wjas0\AppData\Local\T\A\internal\Arduino_Opta_Blueprint_0.2.2_628e3ba029d0e5fe\Arduino_Opta_Blueprint\src\OptaCrc.cpp \
- C:\Users\wjas0\AppData\Local\T\A\internal\Arduino_Opta_Blueprint_0.2.2_628e3ba029d0e5fe\Arduino_Opta_Blueprint\src\OptaCrc.h
